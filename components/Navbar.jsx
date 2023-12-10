e394e2eae7ccd7c5fa30a29db44de4932f67bcf5
Brandon Tieu <brandontieu626@gmail.com>
2023-12-10 00:45:14 -0800

    Worked on navbar, added links and search bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,31 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <nav className="nav_container">
-      <div className="nb_container">Beead</div>
-    </nav>
+    <div className="nav_container">
+      <div className="nb_container">
+        <div>
+          <Link href="/">Beead</Link>
+        </div>
+        <nav className="nb_list_container">
+          <ul className="nb_list">
+            <li>
+              <Link href="/music" className="nb_link">
+                MUSIC
+              </Link>
+            </li>
+            <li>
+              <Link href="/newreleases">NEW RELEASES</Link>
+            </li>
+            <li>
+              <form>
+                <input type="text" placeholder="Search music"></input>
+              </form>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
